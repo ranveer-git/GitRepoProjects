30c823db3b956315be57ac1363488822cd90ce12
ranveer-git <ranveer-git@users.noreply.github.com>
2016-02-15 18:24:03 -0600

    First Commit

--- JavaLearning/src/main/java/com/kgoi/JavaLearning/DeadLock.java
+++ JavaLearning/src/main/java/com/kgoi/JavaLearning/DeadLock.java
@@ -28,7 +28,7 @@ public class DeadLock {
    }
    private static class ThreadDemo2 extends Thread {
       public void run() {
-         synchronized (Lock2) {
+         synchronized (Lock2  ) {
             System.out.println("Thread 2: Holding lock 2...");
             try { Thread.sleep(10); }
             catch (InterruptedException e) {}
